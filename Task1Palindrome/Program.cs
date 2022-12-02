151b50307ddf6298bb13fdca711269e0272aa226
Yana Yakovleva <yana.e.yakovleva@yandex.ru>
2022-12-03 01:48:07 +0300

    first commit

---
+++ Task1Palindrome/Program.cs
@@ -0,0 +1,19 @@
+﻿//
+Console.WriteLine("Введите число: ");
+bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+if (!isParsed){Console.WriteLine("некорректное число"); return;}
+else {return number;}
+
+int[] array = number.Length; // Как массиву присвоить значение введенного числа?
+
+if (number == 5)
+{
+    if (array[0] == array[4] && array[1] == array[3])
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }}
+
---
+++ Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N. Задание должно быть выполнено в методе и метод должен вернуть массив чисел.
+int Number()
+{
+    Console.WriteLine("Введите число  ");
+    bool isNum = int.TryParse(Console.ReadLine(), out int x);
+    if (isNum)
+    {
+        return x;
+    }
+    else
+    {
+        Console.WriteLine("Некорректное значение");
+        return -1;
+    }
+}
+
+int x = Number();
+
+int[] GetCube (int x)
+{
+    int[] array = new int[x];
+    for (int i = 0; i < x; i++)
+    {
+        array[i] = (int)((Math.Pow(i + 1, 3)));
+    }
+    return array;    
+}
+
+int[] Table = GetCube(x);
+
+void PrintArray (int[] arr)
+{
+    for (int i =0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+PrintArray(Table);
